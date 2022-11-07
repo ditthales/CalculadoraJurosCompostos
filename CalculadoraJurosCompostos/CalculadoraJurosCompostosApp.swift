ee9292261493ec34161cdaf34c9b232459445a2e
ditthales <tvgf@academy.cin.ufpe.br>
2022-11-07 08:58:53 -0300

    first commit

---
+++ CalculadoraJurosCompostos/CalculadoraJurosCompostosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraJurosCompostosApp.swift
+//  CalculadoraJurosCompostos
+//
+//  Created by ditthales on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraJurosCompostosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
